bcd3059433ae6bf60ed79e52d4b77d14bab2a630
Rachel Fish <itsrachelfish@gmail.com>
2015-10-27 23:13:55 -0600

    Cleaned up unnecessary laravel files

--- laravel/app/Http/Kernel.php
+++ laravel/app/Http/Kernel.php
@@ -26,8 +26,5 @@ class Kernel extends HttpKernel
      * @var array
      */
     protected $routeMiddleware = [
-        'auth' => \App\Http\Middleware\Authenticate::class,
-        'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
-        'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
     ];
 }
