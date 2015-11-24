60f5bffd6b15f92b584ca6eae26df22addf50a48
Rachel Fish <itsrachelfish@gmail.com>
2015-11-23 19:16:20 -0700

    Updated bootstrap to use self-hosted CSS (LOL AIRPLANES)

--- laravel/resources/views/app.blade.php
+++ laravel/resources/views/app.blade.php
@@ -4,8 +4,8 @@
         <title>Volunteer Database</title>
 
         <!-- Bootstrap styles -->
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" crossorigin="anonymous">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css" crossorigin="anonymous">
+        <link rel="stylesheet" href="/css/bootstrap.min.css" crossorigin="anonymous">
+        <link rel="stylesheet" href="/css/bootstrap-theme.min.css" crossorigin="anonymous">
 
         <!-- Custom styles -->
         <link rel="stylesheet" href="/css/main.css">
