bfcb426392665d5bea6f7e1af7188e7e624a57aa
Rachel Fish <itsrachelfish@gmail.com>
2017-02-05 10:50:08 -0700

    Added validation rules for new schedule fields

--- laravel/app/Http/Requests/ScheduleRequest.php
+++ laravel/app/Http/Requests/ScheduleRequest.php
@@ -27,11 +27,14 @@ class ScheduleRequest extends Request
         [
             'department_id' => 'required|integer|exists:departments,id',
             'shift_data_id' => 'required|integer|exists:shift_data,id',
-            'start_date' => 'date_format:Y-m-d',
-            'end_date' => 'date_format:Y-m-d',
+            'dates' => 'required',
             'start_time' => 'required|time',
             'end_time' => 'required|time',
-            'duration' => 'required|date_format:h:i',
+            'duration' => 'required',
+            'custom_start_time' => 'time',
+            'custom_end_time' => 'time',
+            'custom_duration' => 'date_format:h:i',
+            'volunteers' => 'required|integer',
         ];
     }
 }
