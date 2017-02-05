9167192ad8ad01a2ed4b1ffd97319e514bef125e
Rachel Fish <itsrachelfish@gmail.com>
2017-02-05 06:26:35 -0700

    New migration to store schedule dates

---
+++ laravel/database/migrations/2017_02_05_132520_add_dates_to_shifts.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddDatesToShifts extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('shifts', function ($table)
+        {
+            $table->json('dates');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('shifts', function ($table)
+        {
+            $table->dropColumn('dates');
+        });
+    }
+}
