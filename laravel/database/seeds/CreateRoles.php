7fbcbcd204352ff820cafe1428e0a8766b1be490
Rachel Fish <itsrachelfish@gmail.com>
2017-02-26 22:50:44 -0700

    Seeder to create default roles

---
+++ laravel/database/seeds/CreateRoles.php
@@ -0,0 +1,55 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\Role;
+
+class CreateRoles extends Seeder
+{
+    private $roles =
+    [
+        'admin',
+        'volunteer',
+        'medical',
+        'fire',
+        'banned',
+        'ranger',
+        'ranger-khaki',
+        'event-admin',
+        'department-lead',
+    ];
+
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        foreach($this->roles as $name)
+        {
+            try
+            {
+                $role = new Role;
+                $role->name = $name;
+                $role->save();
+
+                dump("New role created: {$name}");
+            }
+            catch(Exception $exception)
+            {
+                // Get the MySQL error number
+                $error = $exception->getPrevious()->errorInfo[1];
+
+                // Duplicate?
+                if($error == 1062)
+                {
+                    dump("Role already exists: {$name}");
+                }
+                else
+                {
+                    dump("MySQL Error!", $exception);
+                }
+            }
+        }
+    }
+}
