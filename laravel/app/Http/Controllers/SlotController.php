b731f04e81f498502d243a2ae767e7315b48f29f
Rachel Fish <itsrachelfish@gmail.com>
2017-04-17 05:36:58 -0600

    Added password field to slot take page

--- laravel/app/Http/Controllers/SlotController.php
+++ laravel/app/Http/Controllers/SlotController.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\Controller;
 
 use App\Http\Requests\SlotRequest;
 use App\Models\Slot;
+use App\Models\UserRole;
 
 use Illuminate\Support\Facades\Auth;
 
@@ -65,31 +66,57 @@ class SlotController extends Controller
     // Add yourself to an existing slot
     public function take(SlotRequest $request, Slot $slot)
     {
-        if(!$this->userAllowed($slot, 'take'))
+        // Error handling
+        if($slot->schedule->password)
         {
-            $request->session()->flash('error', 'This shift is only available to certain user groups, your account must be approved by an administrator before signing up.');
+            // TODO: Succeptable to timing attacks? lol
+            if($request->get('password') != $slot->schedule->password)
+            {
+                $request->session()->flash('error', 'The password you entered is incorrect. Please contact your department lead to make sure you have the right password.');
+                return redirect()->back();
+            }
         }
         else
         {
-            if($this->eventHasPassed($slot))
+            if(!$this->userAllowed($slot, 'take'))
             {
-                $request->session()->flash('error', 'This event has already passed, you are no longer able to sign up for shifts.');
+                $request->session()->flash('error', 'This shift is only available to certain user groups, your account must be approved by an administrator before signing up.');
+                return redirect()->back();
             }
-            else
+        }
+
+        if($this->eventHasPassed($slot))
+        {
+            $request->session()->flash('error', 'This event has already passed, you are no longer able to sign up for shifts.');
+            return redirect()->back();
+        }
+
+        // Has somebody else already taken this slot?
+        if(is_null($slot->user))
+        {
+            $slot->user_id = Auth::user()->id;
+            $slot->save();
+
+            event(new SlotChanged($slot, ['status' => 'taken', 'name' => Auth::user()->name]));
+            $request->session()->flash('success', 'You signed up for a volunteer shift.');
+
+            // If a password was used
+            if($slot->schedule->password)
             {
-                if(is_null($slot->user))
+                // Assign the user to any roles this shift requires
+                $roles = [];
+
+                foreach($slot->schedule->getRoles() as $role)
                 {
-                    $slot->user_id = Auth::user()->id;
-                    $slot->save();
-                    
-                    event(new SlotChanged($slot, ['status' => 'taken', 'name' => Auth::user()->name]));
-                    $request->session()->flash('success', 'You signed up for a volunteer shift.');
+                    $roles[] = $role->name;
                 }
-                else
-                {
-                    $request->session()->flash('error', 'This slot has already been taken by someone else.');
-                }
+
+                UserRole::assign(Auth::user(), $roles);
             }
+        }
+        else
+        {
+            $request->session()->flash('error', 'This slot has already been taken by someone else.');
         }
         
         return redirect('/event/' . $slot->event->id);
