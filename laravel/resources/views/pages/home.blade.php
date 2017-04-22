7b1bc9804c4d4f6132b157e7b0a22aa019cf7a59
Rachel Fish <itsrachelfish@gmail.com>
2017-04-22 03:00:33 -0600

    Updated home page to announce public release

--- laravel/resources/views/pages/home.blade.php
+++ laravel/resources/views/pages/home.blade.php
@@ -8,12 +8,24 @@
         </p>
 
         <p>
-            Registration is currently open for <b>department leads</b> and members of <b>ignition</b>.
+            Registration is <b>currently open</b> for all Apogaea participants.
+            <b>It is so fun to volunteer!</b>
+            And seriously, it is the cool thing to do.
+            Coordinate with your camp and friends to be a group during a shift. Costume themes highly encouraged!
         </p>
 
         <p>
-            If you are an Apogaea participant and want to start signing up for shifts, please check back in a couple weeks once everything is setup.
+            If you have a ticket for Apogaea you can sign up now and register for some shifts.
+            If you don't have a ticket yet you can still get a guaranteed ticket* by volunteering for certain shifts.
+            <a href="http://apogaea.com/get-involved/volunteer">Contact department leads for more info!</a>
         </p>
+
+
+        <div class="fine-print">
+            * Although there are volunteer tickets available, you will need to go through the specific department in order to make arrangements for qualifying for one of those tickets.
+            Signing up here will not qualify you for a ticket.
+            If a shift is password or role protected (i.e. Medical), contact the department lead to get the passcode or give you permissions for that shift.
+        </div>
 
         <hr>
         
