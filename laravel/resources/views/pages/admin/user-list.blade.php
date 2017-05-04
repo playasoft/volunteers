a333622ee31df15601cb1d6a4374ba6453f6151c
Rachel Fish <itsrachelfish@gmail.com>
2017-05-04 01:25:23 -0600

    Added full name and burner name to user list

--- laravel/resources/views/pages/admin/user-list.blade.php
+++ laravel/resources/views/pages/admin/user-list.blade.php
@@ -7,7 +7,9 @@
     <table class="table table-hover">
         <thead>
             <tr>
-                <th>Name</th>
+                <th>Username</th>
+                <th>Full Name</th>
+                <th>Burner Name</th>
                 <th>Email</th>
                 <th>Roles</th>
                 <th>Date Registered</th>
@@ -18,6 +20,8 @@
             @foreach($users as $user)
                 <tr>
                     <td><a href="/user/{{ $user->id }}">{{ $user->name }}</a></td>
+                    <td>{{ $user->data->full_name or '' }}</td>
+                    <td>{{ $user->data->burner_name or '' }}</td>
                     <td>{{ $user->email }}</td>
                     <td>{{ implode(", ", $user->getRoleNames()) }}</a></td>
                     <td>{{ $user->created_at->format('Y-m-d') }}</td>
