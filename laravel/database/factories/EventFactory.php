b56af1e00149bf34720bf858a0f87c6a37cb173b
Alice <Alice-S@users.noreply.github.com>
2017-12-03 14:48:09 +0000

    Actually do EventFactory defaults right this time

--- laravel/database/factories/EventFactory.php
+++ laravel/database/factories/EventFactory.php
@@ -3,13 +3,13 @@
 use Carbon\Carbon;
 use \App\Models\Event;
 
-$factory->define(Event::class, function (Faker\Generator $faker) {
-    static $start;
+$factory->define(Event::class, function (Faker\Generator $faker, $data) {
+    $start_date = $data['start_date'];
     return [
         'name' => $faker->sentence(2),
         'description' => $faker->paragraph(),
         'image' => '',
-        'start_date' => $start ?: $start = Carbon::now(),
-        'end_date' => $start->copy()->addWeek()
+        'start_date' => $start_date ?: $start_date = Carbon::now(),
+        'end_date' => $start_date->copy()->addWeek()
     ];
 });
