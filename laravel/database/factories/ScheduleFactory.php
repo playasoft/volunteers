7af01a1bea958b229dcb442b9e2e977c588fc40d
Alice <Alice-S@users.noreply.github.com>
2017-12-03 16:45:57 +0000

    Added User & UserData generation to EventSeeder
    refactor this later to allow for multiple events of various dates

--- laravel/database/factories/ScheduleFactory.php
+++ laravel/database/factories/ScheduleFactory.php
@@ -11,8 +11,8 @@ $factory->define(Schedule::class, function (Faker\Generator $faker, $data) {
         'start_date' => $dates[0],
         'end_date' => end($dates),
         'start_time' => Carbon::createFromFormat('H', $faker->numberBetween(0,10))->toTimeString(),
-        'end_time' => Carbon::createFromFormat('H', $faker->numberBetween(14,24))->toTimeString(),
+        'end_time' => Carbon::createFromFormat('H', $faker->numberBetween(14,23))->toTimeString(),
         'duration' => Carbon::createFromFormat('H', $faker->numberBetween(1,4))->toTimeString(),
-        'volunteers' => $faker->numberBetween(1, 10)
+        'volunteers' => $faker->numberBetween(1, 3)
     ];
 });
--- laravel/database/seeds/EventSeeder.php
+++ laravel/database/seeds/EventSeeder.php
@@ -9,6 +9,8 @@ use App\Models\Department;
 use App\Models\Shift;
 use App\Models\Schedule;
 use App\Models\Slot;
+use App\Models\User;
+use App\Models\UserData;
 
 class EventSeeder extends Seeder
 {
@@ -21,20 +23,42 @@ class EventSeeder extends Seeder
     {
         // Create an Event
         $event = factory(Event::class)->create();
+        dump("Created Event: " . $event->name);
+
         // Create a Department and assign it the Event
         $department = factory(Department::class)->create(['event_id' => $event->id]);
+        dump("Created Department: " . $department->name);
+
         // Create a Shift and assign it to the Event and Department
         $shift = factory(Shift::class)->create(['event_id' => $event->id, 'department_id' => $department->id]);
+        dump("Created Shift: " . $shift->name);
+
         // Create a list of dates between the start and end date of the event
         $dates = [];
         foreach ($event->days() as $day)
         {
             $dates[] = $day->date->toDateString();
         }
+
         // Create a Schedule and assign it to the Department
         $schedule = factory(Schedule::class)->create(['department_id' => $department->id, 'shift_id' => $shift->id, 'dates' => json_encode($dates)]);
+        dump("Created Schedule: " . $schedule->id . " in department: " . $schedule->department->name . " for shift: " . $schedule->shift->name);
+
         // Generate slots for the schedule
         Slot::generate($schedule);
+        dump("Generating slots for schedule: " . $schedule->id);
+        
+        // Create and assign Users to random slots.
+        dump("Creating users and assigning to slots");
+        $slots = Slot::where('schedule_id', $schedule->id)->get();
+        foreach($slots as $slot)
+        {
+            $user = factory(User::class)->create();
+            factory(UserData::class)->create(['user_id' => $user->id]);
+            $slot->user_id = $user->id;
+            $slot->save(); 
+        }
+        dump("Done!");
     }
 
 }
