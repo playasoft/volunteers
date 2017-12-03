25c96afd368ce447a3afeb8576b7e3fe234e9225
Alice <Alice-S@users.noreply.github.com>
2017-12-03 14:17:22 +0000

    Adjusted EventFactory start_date default

--- laravel/database/factories/EventFactory.php
+++ laravel/database/factories/EventFactory.php
@@ -4,11 +4,12 @@ use Carbon\Carbon;
 use \App\Models\Event;
 
 $factory->define(Event::class, function (Faker\Generator $faker) {
+    static $start;
     return [
         'name' => $faker->sentence(2),
         'description' => $faker->paragraph(),
         'image' => '',
-        'start_date' => Carbon::now(),
-        'end_date' => Carbon::now()->addWeeks(1)
+        'start_date' => $start ?: $start = Carbon::now(),
+        'end_date' => $start->addWeeks(1)
     ];
 });
