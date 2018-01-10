dc6e8c0fda4210225e44223d38f5afec16d52d63
Rachel Fish <itsrachelfish@gmail.com>
2018-01-10 02:07:38 -0700

    Replaced learn more with login button on home page

--- laravel/resources/views/pages/home.blade.php
+++ laravel/resources/views/pages/home.blade.php
@@ -7,9 +7,9 @@
         {!! env('SITE_DESCRIPTION', '[Site description goes here]') !!}
 
         <hr>
-        
+
         <p>
-            <a class="btn btn-primary btn-lg" href="/about" role="button">Learn More</a>
+            <a class="btn btn-primary btn-lg" href="/login" role="button">Login</a>
             <a class="btn btn-success btn-lg" href="/register" role="button">Register an Account</a>
         </p>
     </div>
