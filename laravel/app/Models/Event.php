f0ea23d4d845361d6d7181a0857207a441995980
Stephen Crim <stephen@criminally.sexy>
2019-01-26 20:01:06 -0700

    Fix for issue #130
    There may be a more elegant way to do this, but the gist is that we pluck all of the dates for the event's shifts, concatenate them together, and then use array_unique to remove duplicates. Then, instead of testing for a start_date, we just see if the current $date is present in that list.

--- laravel/app/Models/Event.php
+++ laravel/app/Models/Event.php
@@ -106,15 +106,29 @@ class Event extends Model
         // This only works when the start date is before the end date
         if($start_date->lte($end_date))
         {
-            // $date keeps track of the current date as we loop towards the end
+            // do this once
+            if($hideEmpty)
+            {
+                // get an array of all the dates fields for this event
+                $shift_dates = Schedule::whereIn('shift_id', $this->shifts->pluck('dates'));
+                $merged_dates = [];
+                //iterate through scheduled dates and merge them into one list
+                foreach ($shift_dates as $i) {
+                    $merged_dates = array_merge($merged_dates, $i);
+                }
+                // remove duplicate dates
+                $shift_dates = array_unique( $merged_dates );
+            }
+            
+			// $date keeps track of the current date as we loop towards the end
             $date = $start_date;
 
             while($date->lte($end_date))
             {
                 if($hideEmpty)
                 {
-                    // Pluck all of the shift IDs for this event and check the if any in the schedule start today
-                    if(Schedule::whereIn('shift_id', $this->shifts->pluck('id'))->where('start_date', $date->format('Y-m-d'))->get()->isEmpty())
+                    // check to see if the current day is in the list of shift_dates we prepared earlier
+                    if(in_array( $date->format('Y-m-d'), $shift_dates ))
                     {
                         // Continue onto the next day
                         $date->addDay();
