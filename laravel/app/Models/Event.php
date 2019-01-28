c2b112bb4d07a32f3b571417347c9aaea701b7b6
Stephen Crim <stephen@criminally.sexy>
2019-01-28 16:53:45 -0700

    additional fixes for #130 and #132
    +pluck dates, not id's.
    +json_decode the dates
    +remember to negate the in_array test so that it does what we want
    +also fleshed out two private functions to specifically handle the corner cases for the custom value clobbering that was happening while exempting the predefined options in the Schedule edit view.

--- laravel/app/Models/Event.php
+++ laravel/app/Models/Event.php
@@ -110,11 +110,11 @@ class Event extends Model
             if($hideEmpty)
             {
                 // get an array of all the dates fields for this event
-                $shift_dates = Schedule::whereIn('shift_id', $this->shifts->pluck('dates'));
+                $shift_dates = Schedule::whereIn('shift_id', $this->shifts->pluck('id')->pluck('dates'));
                 $merged_dates = [];
                 //iterate through scheduled dates and merge them into one list
                 foreach ($shift_dates as $i) {
-                    $merged_dates = array_merge($merged_dates, $i);
+                    $merged_dates = array_merge($merged_dates, json_decode($i));
                 }
                 // remove duplicate dates
                 $shift_dates = array_unique( $merged_dates );
@@ -128,7 +128,7 @@ class Event extends Model
                 if($hideEmpty)
                 {
                     // check to see if the current day is in the list of shift_dates we prepared earlier
-                    if(in_array( $date->format('Y-m-d'), $shift_dates ))
+                    if(!in_array( $date->format('Y-m-d'), $shift_dates ))
                     {
                         // Continue onto the next day
                         $date->addDay();
