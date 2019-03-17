cb5c87dcbca982fbce59d7addba4b952ad780ed7
Meleeman01 <meleeman01@live.com>
2019-03-17 12:38:03 -0500

    create ui for filter option

--- laravel/resources/views/pages/admin/user-list.blade.php
+++ laravel/resources/views/pages/admin/user-list.blade.php
@@ -1,12 +1,27 @@
+<?php 
+    $userRoles = \App\Models\Role::get();
+?>
 @extends('app')
 
 @section('content')
     <h1>Registered Users </h1>
 
-    <form class="input-group user-search" method="GET" action="/users">
-        <input type="text" name="search" class=" form-control" placeholder="Search by email or username" value="{{ $_GET['search'] or '' }}">
-        <div class="input-group-btn">
-            <button type="submit" class=" btn btn-primary"><span class="glyphicon glyphicon-search" aria-hidden="true"></span></button>
+    <form style="display: flex; flex-wrap: wrap; flex-basis: 0; align-items: flex-start; justify-content: space-between;" method="GET" action="/users">
+        
+        <div class="input-group col-sm-8" style="margin-bottom: 1em;">
+            <input type="text" name="search" class=" form-control" placeholder="Search by email or username" value="{{ $_GET['search'] or '' }}">
+        
+            <div class="input-group-btn">
+                <button type="submit" class=" btn btn-primary"><span class="glyphicon glyphicon-search" aria-hidden="true"></span></button>
+            </div>
+        </div>
+        <div class="form-group col-sm-3" style="padding-left:0; ">
+            <select class="form-control">
+                <option value="{{ $_GET['search'] or '' }}">--Filter Permissions--</option>
+                @foreach($userRoles as $role)
+                <option name='{{$role->name}}' value="{{$_GET[$role->name] or ''}}">{{$role->name}}</option>
+                @endforeach
+            </select>
         </div>
     </form>
     <hr>
