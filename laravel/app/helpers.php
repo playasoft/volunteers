917570ec8858a45b78e1439ac19622ff08eeb268
ritualsfinal <solverjt@gmail.com>
2019-05-24 20:56:53 -0600

    [fix] mo' readability, less problems

--- laravel/app/helpers.php
+++ laravel/app/helpers.php
@@ -6,20 +6,28 @@ class Helpers
 {
     /**
      * display name Helper
-     * Return a burner if the given user has one or return an empty string.
-     * If $alt is false and no burner exists, return the user name of the user
-     * instead.
-     * If $alt is anything else and no burner exists, return the supplied $alt.
-     * @param  User   $user   the user (non-null)
-     * @param  string $alt    the alternative replacement for the burner name
-     * @return string         the appropriate display name
+     * Return a burner name or an empty string.
+     * If an alternative is given, it is used instead of the empty string.
+     * If the alternative is a boolean false, it uses the users user name.
+     * @param  User   $user         the user (non-null)
+     * @param  string $alternative  the alternative display name
+     * @return string               the appropriate display name
      */
-    public static function displayName($user, $alt='')
+    public static function displayName($user, $alternative='')
     {
-        return (!is_null($user->data) &&
-            !is_null($user->data->burner_name) &&
-            trim($user->data->burner_name) !== "") ?
-              $user->data->burner_name :
-              (($alt === false) ? $user->name : $alt);
+        // assume the display name will be the alternative name
+        $display_name = $alternative;
+        // if there exists a burner name, use it as the display name instead.
+        if (!is_null($user->data) && !is_null($user->data->burner_name)) {
+            if (trim($user->data->burner_name) !== "") {
+                $display_name = $user->data->burner_name;
+            }
+        }
+        // if there isn't a burner name, use the user name instead if there is
+        // no alternative ($alternative === false)
+        if ($display_name === $alternative && $alternative === false) {
+            $display_name = $user->name;
+        }
+        return $display_name;
     }
 }
