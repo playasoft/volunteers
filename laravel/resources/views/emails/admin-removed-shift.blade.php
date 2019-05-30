0c63c74c7c196e1a3c0ab8d7b27f8d0ae5f0cd8e
ritualsfinal <solverjt@gmail.com>
2019-05-29 21:41:29 -0600

    [docs] some readability added for mailing user on removal

--- laravel/resources/views/emails/admin-removed-shift.blade.php
+++ laravel/resources/views/emails/admin-removed-shift.blade.php
@@ -2,5 +2,8 @@
 
 <p>
     It seems you've been effected by a recent scheduling change for {{ $slot->schedule->shift->name }}!
-    If you'd like reshedule, click here: <a href="{{ env('SITE_URL').'/event/'.$slot->schedule->department->event->id }} ">{{ env('SITE_NAME') }}</a>.
+    If you'd like reshedule, click here:
+      <a href="{{ env('SITE_URL').'/event/'.$slot->schedule->department->event->id }}">
+        {{ env('SITE_NAME') }}
+      </a>.
 </p>
