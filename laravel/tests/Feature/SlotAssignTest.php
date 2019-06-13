1bf2ceb1d0f5fdc53a9fd3c5b1a83f5dd120d942
James Thornton <solverjt@gmail.com>
2019-06-13 15:22:56 -0600

    [build] more test testing
    + slot factory for quick build
    ~ more robust factories
    ~ a way to shut off env
    + more scripts for testing
    ~ better slot tests

--- laravel/tests/Feature/SlotAssignTest.php
+++ laravel/tests/Feature/SlotAssignTest.php
@@ -2,19 +2,67 @@
 
 namespace Tests\Feature;
 
-use Tests\TestCase;
-use Illuminate\Foundation\Testing\WithFaker;
+use App\Models\Department;
+use App\Models\Event;
+use App\Models\Schedule;
+use App\Models\Shift;
+use App\Models\Slot;
+use App\Models\User;
+use App\Models\UserData;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
 
 class SlotAssignTest extends TestCase
 {
+    use RefreshDatabase;
+
     /**
-     * A basic test example.
+     * [setUpBeforeClass description]
+     */
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->user = factory(User::class)->create();
+        factory(UserData::class)->create([
+            'user_id' => $this->user->id
+        ]);
+        $this->event = factory(Event::class)->create();
+        $this->department = factory(Department::class)->create();
+        $this->shift = factory(Shift::class)->create();
+        $this->schedule = factory(Schedule::class)->create();
+        $this->slot = factory(Slot::class)->create();
+    }
+
+    /**
      * @test
+     * A basic test example.
+     *
      * @return void
      */
-    public function testExample()
+    public function user_can_see_a_slot()
     {
-        $this->assertTrue(true);
+        $response = $this->actingAs($this->user)
+            ->get('/slot/' . $this->slot->id . '/view');
+
+        $response->assertStatus(200);
+    }
+
+    /**
+     * @test
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function user_can_take_a_slot()
+    {
+        $response = $this->actingAs($this->user)
+            ->post('/slot/' . $this->slot->id . '/take');
+
+        var_dump($response->getContent());
+
+        $response->assertRedirect('/');
+
+        $response->assertJson(['warning' => $this->slot->id]);
     }
 }
