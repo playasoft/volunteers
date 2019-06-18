dc77b4fbe7db93321815b8af5307b10bed8cc2bd
James Thornton <solverjt@gmail.com>
2019-06-17 18:06:50 -0600

    [dependency] updated laravel from 5.6.12 to 5.6.16 for some features

--- laravel/database/factories/UserFactory.php
+++ laravel/database/factories/UserFactory.php
@@ -19,3 +19,26 @@ $factory->state(User::class, 'with_setup', function (Faker $faker)
         [
     ];
 });
+
+$factory->state(User::class, 'admin', function (Faker $faker)
+{
+    return
+        [
+    ];
+});
+
+$factory->afterCreatingState(User::class, 'admin', function (User $user, Faker $faker) {
+    //generate the admin role
+    $admin_role = Role::where('name', 'admin')->first();
+    if (!$admin_role)
+    {
+        $admin_role = factory(Role::class)->create([
+            'name' => 'admin',
+        ]);
+    }
+
+    $user->roles()->save(factory(UserRole::class)->make([
+        'role_id' => $admin_role->id,
+        'user_id' => $user->id,
+    ]));
+});
--- laravel/database/factories/UserUploadFactory.php
+++ laravel/database/factories/UserUploadFactory.php
@@ -23,22 +23,5 @@ $factory->state(UserUpload::class, 'with_setup', function (Faker $faker)
         {
             return factory(User::class)->states('with_setup')->create()->id;
         },
-        'admin_id' => function ()
-        {
-            //generate the admin role
-            $admin_role = Role::where('name', 'admin')->first();
-            if (!$admin_role)
-            {
-                $admin_role = factory(Role::class)->create([
-                    'name' => 'admin',
-                ]);
-            }
-
-            $user = factory(User::class)->states('with_setup')->create();
-            $admin = $user->roles()->save(factory(UserRole::class)->make([
-                'role_id' => $admin_role->id,
-                'user_id' => $user->id,
-            ]));
-        },
     ];
 });
