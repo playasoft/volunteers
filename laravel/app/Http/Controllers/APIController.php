9100e018cebd8957790120b1186328465451e021
demogorgonzola <solverjt@gmail.com>
2019-08-21 17:58:05 -0600

    [wip] all endpoints and tests up to shifts ok

--- laravel/app/Http/Controllers/APIController.php
+++ laravel/app/Http/Controllers/APIController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
+use App\Models\Event;
+
 /**
  * TODO: add auth bullshit
  */
@@ -15,18 +17,7 @@ class APIController extends Controller
      */
     public function profile(Request $request)
     {
-        /**
-         * {
-         * "username": "rachel",
-         * "email": "rachel@wetfish.net",
-         * "permissions": ["volunteer", "admin", "ranger"],
-         * "full_name": "Rachel Fish",
-         * "burner_name": "BlubBlub",
-         * "phone_number": "555-123-4567"
-         * }
-         */
         $user = Auth::user();
-        // $user_data = $user->data;
         return response()->json([
             'username' => $user->name,
             'email' => $user->email,
@@ -42,7 +33,9 @@ class APIController extends Controller
      */
     public function events(Request $request)
     {
-        // TODO
+        $events = Event::select('id', 'name', 'start_date', 'end_date')->get();
+
+        return response()->json($events->toArray());
     }
 
     /**
@@ -50,7 +43,10 @@ class APIController extends Controller
      */
     public function departments(Request $request, $id)
     {
-        // TODO
+        $event = Event::find($id);
+        $departments = $event->departments()->select('id', 'name')->get();
+
+        return response()->json($departments->toArray());
     }
 
     /**
@@ -59,6 +55,11 @@ class APIController extends Controller
     public function roles(Request $request, $id)
     {
         // TODO
+
+        $event = Event::find($id);
+        $roles = $event->shifts()->select('id', 'department_id', 'name')->get();
+
+        return response()->json($roles->toArray());
     }
 
     /**
@@ -67,6 +68,30 @@ class APIController extends Controller
     public function shifts(Request $request, $id)
     {
         // TODO
+
+        $event = Event::find($id);
+        $data = DB::table('slots')
+            ->leftJoin('schedule', 'slots.schedule_id', '=', 'schedule.id')
+            ->leftJoin('departments', 'schedule.department_id', '=', 'departments.id')
+            ->leftJoin('events', 'departments.event_id', '=', 'events.id')
+            ->leftJoin('users', 'slots.user_id', '=', 'users.id')
+            ->leftJoin('user_data', 'user_data.user_id', '=', 'users.id')
+            ->leftJoin('event_roles', 'events.id', '=', 'event_roles.id')
+            ->leftJoin('roles', 'event_roles.role_id', '=', 'roles.id')
+            ->whereNotNull('slots.user_id')
+            ->select(
+                'events.id', 
+                'schedule.department_id',
+                // 'schedule.id', //replace with role_id
+                'slots.start_time',
+                'slots.end_time',
+                'slots.user_id',
+                'users.email',
+                'user_data.full_name',
+                'slots.status'
+            )->get();
+
+        return response()->json($data->toArray());
     }
 
 }
--- laravel/tests/Feature/APIControllerTest.php
+++ laravel/tests/Feature/APIControllerTest.php
@@ -10,6 +10,10 @@ use App\Models\User;
 use App\Models\UserData;
 use App\Models\UserRole;
 use App\Models\Role;
+use App\Models\Event;
+use App\Models\Department;
+use App\Models\Shift;
+use App\Models\Slot;
 
 
 class APIControllerTest extends TestCase
@@ -21,7 +25,7 @@ class APIControllerTest extends TestCase
      *
      * @return void
      */
-    public function profile_enpoint_returns_right_keys()
+    public function profile_endpoint_returns_right_keys()
     {
         // Given    
         $user = factory(User::class)->states('admin')->create([
@@ -46,5 +50,139 @@ class APIControllerTest extends TestCase
             'phone_number' => '(123) 456-7890',
             'permissions' => ['admin'],
         ]);
+    }
+
+    /**
+     * @test
+     * 
+     * @return void
+     */
+    public function events_endpoint_returns_right_keys()
+    {
+        // Given 
+        $user = factory(User::class)->states('admin')->create();
+        $event1 = factory(Event::class)->create([
+            'name' => 'CoolCon'
+        ]);
+        $event2 = factory(Event::class)->create([
+            'name' => 'LameCon',
+        ]);
+
+        // When 
+        $response = $this->actingAs($user)->get('/v1/events');
+
+        // Then 
+        $response->assertJson([
+            [
+                'id' => $event1->id,
+                'name' => $event1->name,
+                'start_date' => $event1->start_date,
+                'end_date' => $event1->end_date,
+            ],
+            [
+                'id' => $event2->id,
+                'name' => $event2->name,
+                'start_date' => $event2->start_date,
+                'end_date' => $event2->end_date,
+            ],
+        ]);
+    }
+
+    /**
+     * @test
+     * 
+     * @return void
+     */
+    public function departments_endpoint_returns_right_keys()
+    {
+        // Given 
+        $user = factory(User::class)->create();
+        $event = factory(Event::class)->create();
+        $departments = factory(Department::class,2)->create([
+            'event_id' => $event->id,
+        ]);
+
+        // Then 
+        $response = $this->actingAs($user)->get("/v1/event/{$event->id}/departments");
+
+        // When 
+        $response->assertJson([
+            [
+                'id' => $departments[0]->id,
+                'name' => $departments[0]->name,
+            ],
+            [
+                'id' => $departments[1]->id,
+                'name' => $departments[1]->name,
+            ],
+        ]);
+    }
+
+    /**
+     * @test
+     * 
+     * @return void
+     */
+    public function roles_endpoint_returns_right_keys()
+    {
+        // Given 
+        $user = factory(User::class)->create();
+        $event = factory(Event::class)->create();
+        $department = factory(Department::class)->create([
+            'event_id' => $event->id,
+        ]);
+        $roles = factory(Shift::class,2)->create([
+            'event_id' => $event->id,
+            'department_id' => $department->id,
+        ]);
+
+        // Then 
+        $response = $this->actingAs($user)->get("/v1/event/{$event->id}/roles");
+
+        // When 
+        $response->assertJson([
+            [
+                'id' => $roles[0]->id,
+                'department_id' => $department->id,
+                'name' => $roles[0]->name,
+            ],
+            [
+                'id' => $roles[1]->id,
+                'department_id' => $department->id,
+                'name' => $roles[1]->name,
+            ],
+        ]);
+    }
+
+    /**
+     * @test
+     * 
+     * @return void
+     */
+    public function shifts_endpoint_returns_right_keys()
+    {
+        // Given 
+        $user = factory(UserData::class)->create()->user;
+        $shift = factory(Slot::class)->create([
+            'user_id' => $user->id,
+        ]);
+
+
+        // Then 
+        $response = $this->actingAs($user)->get("/v1/event/{$event->id}/shifts");
+
+        // When 
+        // $response->assertJson([
+        //     [
+        //         'id' => $shift->id,
+        //         'department_id' => $shift->department->id,
+        //     ],
+        //     [
+        //         'id' => $roles[1]->id,
+        //         'department_id' => $department->id,
+        //         'name' => $roles[1]->name,
+        //     ],
+        // ]);
+        dd($response);
     }
 }
