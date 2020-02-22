41e132cacdf018371ce2497ad2cbd4550f88d3a0
Tompiosh <61041765+Tompiosh@users.noreply.github.com>
2020-02-21 18:38:31 -0700

    added more information text

--- laravel/resources/views/emails/user-shift-confirmation.blade.php
+++ laravel/resources/views/emails/user-shift-confirmation.blade.php
@@ -3,6 +3,9 @@
     Your shift will take place on {{ $start_date}} between {{ $start_time }} and {{ $end_time }}.
 </p>
 <p>
+    For volunteer ticketing information, please visit the {{ $event_name }} ticketing webpage. 
+</p>
+<p>
     If you did NOT sign up for this shift or would like to CANCEL this shift, <a href="{{ env('SITE_URL').'/slot/'.$slot->id.'/view' }}">click here</a>.
 </p>
 <p>
