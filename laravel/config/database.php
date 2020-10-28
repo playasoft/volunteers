64a188fc8f74531dbbbb62760f5126965760653a
halfwit <michaelmisch1985@gmail.com>
2020-10-27 19:11:55 -0700

    Set db explicitly

--- laravel/config/database.php
+++ laravel/config/database.php
@@ -54,7 +54,7 @@ return [
 
         'mysql' => [
             'driver'    => 'mysql',
-            'host'      => env('DB_HOST', 'localhost'),
+            'host'      => env('DB_HOST', 'db'),
             'database'  => env('DB_DATABASE', 'forge'),
             'username'  => env('DB_USERNAME', 'forge'),
             'password'  => env('DB_PASSWORD', ''),
